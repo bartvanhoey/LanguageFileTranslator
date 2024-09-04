3314d4a21be812795c8b0382002ba20cfe10dd19
VanhoeyB <bart.vanhoey@graw.de>
2024-09-04 12:03:56 +0200

    Update

--- JsonTranslatorApp/gulpfile.js
+++ JsonTranslatorApp/gulpfile.js
@@ -2,10 +2,13 @@ const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgeCss = require('gulp-purgecss')
 
-const root = "./wwwroot/css";
+const rootCss = "./wwwroot/css";
+const root = "./wwwroot";
 
 const bootstrap = "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 const bootstrapIcons = "./node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
+const bootstrapFontsWoff = "./node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff";
+const bootstrapFontsWoff2 = "./node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2";
 
 async function buildStyles() {
     return await src('Sass/**/*.scss')
@@ -20,13 +23,21 @@ async function watchTask() {
 }
 
 async function bootstrapTask() {
-    src([bootstrap]).pipe(dest(root+'/bootstrap/'));
+    src([bootstrap]).pipe(dest(rootCss+'/bootstrap/'));
 }
 
 async function bootstrapIconsTask() {
-    src([bootstrapIcons]).pipe(dest(root+'/bootstrap-icons/'));
+    src([bootstrapIcons]).pipe(dest(rootCss+'/bootstrap-icons/'));
+}
+
+async function bootstrapFontsWoffTask() {
+    src([bootstrapFontsWoff], { encoding: false }).pipe(dest(rootCss+'/bootstrap-icons/fonts/'));
+}
+
+async function bootstrapFontsWoff2Task() {
+    src([bootstrapFontsWoff2], { encoding: false }).pipe(dest(rootCss+'/bootstrap-icons/fonts/'));
 }
 
 
 
-exports.default = series(buildStyles, watchTask, bootstrapTask, bootstrapIconsTask)
+exports.default = series(buildStyles, watchTask, bootstrapTask, bootstrapIconsTask, bootstrapFontsWoffTask, bootstrapFontsWoff2Task)
