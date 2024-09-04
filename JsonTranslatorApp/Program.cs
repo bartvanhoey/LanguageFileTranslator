d53df924da70f42dff15876557d84cd76943ead3
VanhoeyB <bart.vanhoey@graw.de>
2024-09-04 15:50:02 +0200

    Update

--- JsonTranslatorApp/Program.cs
+++ JsonTranslatorApp/Program.cs
@@ -2,7 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using JsonTranslatorApp;
 
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", false, true);
+    
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
