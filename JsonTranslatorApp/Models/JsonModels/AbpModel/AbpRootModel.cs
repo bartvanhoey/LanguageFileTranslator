1d5c44e885b42433a8ca767b572d21bd0cb9de42
ekapituro <ekapituro@gmail.com>
2024-10-01 21:26:34 +0200

    Update

--- JsonTranslatorApp/Models/JsonModels/AbpModel/AbpRootModel.cs
+++ JsonTranslatorApp/Models/JsonModels/AbpModel/AbpRootModel.cs
@@ -1,20 +1,28 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using JsonTranslatorApp.Infra.Extensions;
 
 namespace JsonTranslatorApp.Models.JsonModels.AbpModel;
 
 public class AbpRootModel
 {
     public string culture { get; set; }
-    public Dictionary<string, string> texts { get; set; } = new Dictionary<string, string>();
+    public Dictionary<string, string> texts { get; set; } = new();
 
 }
 
 
 public class AbpRootModelFalse
 {
+    public AbpRootModelFalse(string jam)
+    {
+        if (jam.IsNullOrWhiteSpace()) throw new ArgumentNullException();
+
+        Jam = jam;
+    }
+
     public string Jam { get; set; }
-    public Dictionary<string, string> objects { get; set; } = new Dictionary<string, string>();
+    public Dictionary<string, string> objects { get; set; } = new();
 
 }
 
